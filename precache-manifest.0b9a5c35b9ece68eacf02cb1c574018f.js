18cd7fec3935e489933e3d9cf1706c21a039ba69
sharjeel <torasx3600@gmail.com>
2020-10-03 15:47:02 +0500

    Updates

---
+++ precache-manifest.0b9a5c35b9ece68eacf02cb1c574018f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05ede39119afaa22cc45d3173a44b5b5",
+    "url": "/demo.BitFlexToken/index.html"
+  },
+  {
+    "revision": "85f4ea797fe1ea6093c7",
+    "url": "/demo.BitFlexToken/static/css/main.521915c2.chunk.css"
+  },
+  {
+    "revision": "c988ab1c6c45f19399ae",
+    "url": "/demo.BitFlexToken/static/js/2.b087988a.chunk.js"
+  },
+  {
+    "revision": "fddc5c707eccc23815a8617cf795b106",
+    "url": "/demo.BitFlexToken/static/js/2.b087988a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85f4ea797fe1ea6093c7",
+    "url": "/demo.BitFlexToken/static/js/main.f8376253.chunk.js"
+  },
+  {
+    "revision": "cb8fb93f27b62f304bb6",
+    "url": "/demo.BitFlexToken/static/js/runtime-main.5a0e5111.js"
+  }
+]);
